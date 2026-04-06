fbc59e45fc8c51d61f1c61c0da691d1dd96d54dd
Ferdi Mehmed Akif <76132036+ferdi7o@users.noreply.github.com>
2026-04-06 20:50:02 +0300

    VER: 1.0.0/ ready for deploy

--- feya_world/urls.py
+++ feya_world/urls.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from pages.views import HomeView, AboutPageView
 
+def custom_404(request, exception):
+    return render(request, '404.html', status=404)
+
+def custom_500(request):
+    return render(request, '500.html', status=500)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('', HomeView.as_view(), name='home'),
     path('users/', include('users.urls')),
     path('products/', include('products.urls')),
@@ -17,3 +23,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+handler404 = 'feya_world.urls.custom_404'
+handler500 = 'feya_world.urls.custom_500'
--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from .api_views import ProductListAPIView
 from .views import (
     ProductListView,
     ProductDetailView,
@@ -13,6 +14,7 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('product/add/', ProductCreateView.as_view(), name='product_create'),
+    path('api/list/', ProductListAPIView.as_view(), name='api_product_list'),
     path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
     path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
     path('category/<slug:category_slug>/', views.ProductListView.as_view(), name='product_list_by_category'),
